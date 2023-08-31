f05d614d16ea963fb840bb6a3c00e23b7f5641c7
Hank <hank.tsunghanlin@gmail.com>
2023-08-31 22:00:41 +0800

    awardChanges

--- app.js
+++ app.js
@@ -138,19 +138,19 @@ app.post("/startWinwheel", async function (request, response) {
 	if (randomNumber < 0.55) {
 		// 55%
 		stopAt = (1 + Math.floor((Math.random() * 58)));
-		award = '100元當日折扣';
+		award = '100元折價券';
 	} else if (randomNumber < 0.95) {
 		// 40%
 		stopAt = (181 + Math.floor((Math.random() * 58)));
-		award = '再接再厲';
+		award = '銘謝惠顧';
 	} else if (randomNumber < 0.98) {
 		// 3%
 		stopAt = (241 + Math.floor((Math.random() * 58)));
-		award = '200元當日折扣';
+		award = '200元折價券';
 	} else {
 		// 2%
 		stopAt = (121 + Math.floor((Math.random() * 58)));
-		award = '300元當日折扣';
+		award = '300元折價券';
 	}
  
 	
